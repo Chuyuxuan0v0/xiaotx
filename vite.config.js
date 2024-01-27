50853f2834023d1eb8aaf891fe38c0849e40b6c5
chuyxuan_mbp <mail@chuyuxuan.top>
2024-01-27 23:41:15 +0800

    init: css颜色配置

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,41 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// elment-plus 自动引入
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+
+    // 导入的两个插件
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver(
+        // 告知 element-plus 采用自定义配置sass
+        {importStyle: 'sass'}
+      )],
+    }),
   ],
   resolve: {
+    // 实际的路径转换配置
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
     }
   }
 })
